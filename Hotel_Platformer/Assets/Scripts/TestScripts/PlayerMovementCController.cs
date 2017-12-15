9fe34c45eb3a6b21d455ff3bd22a283763b3d687
Bussler <maarten.bussler@gmail.com>
2017-12-15 16:42:01 +0100

    Character Controller
    Implemented a testplayer who can be moved with the character controller - it's ok imo but maybe we find something better. pls test for yourself.

---
+++ Hotel_Platformer/Assets/Scripts/TestScripts/PlayerMovementCController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementCController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpSpeed;
+    public float gravity;
+    CharacterController playerController;
+    private Vector3 moveDirection;
+
+
+
+    void Start()
+    {
+        playerController = GetComponent<CharacterController>();//Player has to have a charactaercontroller attached in order to make this stuff wörk
+    }
+
+    //Movement in update, since we aren't using a rigidbody but a characterController
+    void Update()
+    {
+
+        if (playerController.isGrounded)//doublejumps not yet implemented, maybe we have to change this because we can't do anything in air
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * moveSpeed;//calculate a vector/movement with given playerinput
+            moveDirection = transform.TransformDirection(moveDirection);
+
+            if (Input.GetButton("Jump"))//jumping
+            {
+                moveDirection.y = jumpSpeed;//setting the y value, therefore making the player jump
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        playerController.Move(moveDirection * Time.deltaTime);//making the player move ingame
+    }
+}
---
+++ Hotel_Platformer/Assets/Scripts/TestScripts/PlayerMovementRigid.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementRigid : MonoBehaviour {
+
+    public float speed;
+    public float jumpspeed;
+    private Rigidbody playerRigidBody;
+    private Vector3 velocity;
+
+	void Start () {
+        playerRigidBody = GetComponent<Rigidbody>();
+        velocity = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Jump"))
+        {
+            jump();
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        //if we use this, we have to make sure that the player stands on the ground first
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        velocity.x = moveHorizontal * speed;
+
+        float moveVertical = Input.GetAxis("Vertical");
+        velocity.z = moveVertical * speed;
+
+        velocity.y = playerRigidBody.velocity.y;
+
+        playerRigidBody.velocity = transform.TransformDirection(velocity);
+    }
+
+    void jump()
+    {
+        playerRigidBody.AddForce(Vector3.up * jumpspeed);
+    }
+
+    
+}
