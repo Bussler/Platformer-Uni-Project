51b03d7c5db805699a9da6d39f5d4132bc3c072b
Luca <34301369+GilgusMaximus@users.noreply.github.com>
2017-12-23 20:30:20 +0100

    Portals
    Added Portalprefabs to the Game.
    When creating a prefab, you also need to create a Render Texture. Tagged the Playerobject with "Player".

---
+++ Hotel_Platformer/Assets/Scripts/Portals/Portal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour {
+
+    public float x;
+    public float y;
+    public float z;
+    private Vector3 teleportKoordinaten;
+
+    private void Start() {
+        teleportKoordinaten = new Vector3(x, y, z);
+    }
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Player") {    //Wenn ein Spieler hindurchläuft, dann wird er teleportiert
+			other.transform.position = teleportKoordinaten;
+		}
+	}
+}
