d5c4f136d2e76cd4fcaa4f8afd9060fc6f68337a
Luca <34301369+GilgusMaximus@users.noreply.github.com>
2018-01-09 00:10:35 +0100

    New Portal Mechanic
    Added Brackeys Portal Technique into one direction, bc we do not need both ways. Problem: With 3rd Person Camera, it does not work that good, because we got up and down Movement of the Camera, which destroys the effect. Will eventually work when 1st Person Camera is added

---
+++ Hotel_Platformer/Assets/PortalTextureSetup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTextureSetup : MonoBehaviour {
+
+    public Camera cameraB;
+    public Material material;
+
+
+	// Use this for initialization
+	void Start () {
+		if(cameraB.targetTexture != null) {
+            cameraB.targetTexture.Release();
+        }
+        cameraB.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        material.mainTexture = cameraB.targetTexture;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
