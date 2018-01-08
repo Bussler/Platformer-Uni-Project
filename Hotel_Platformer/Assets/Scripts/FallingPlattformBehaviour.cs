d1612189a4998c38e42e0e9f9bced41a02a1d14b
Bussler <maarten.bussler@gmail.com>
2018-01-08 16:57:39 +0100

    platform
    platforms work, but sticking to them doesn't

---
+++ Hotel_Platformer/Assets/Scripts/FallingPlattformBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlattformBehaviour : MonoBehaviour {
+
+    public float timeBeforeDownFall;
+    public float fallDownSpeed;
+    public float whenToDestroy;
+    private bool playerentered = false;
+    private Vector3 originalPosition;
+
+    private void Start()
+    {
+        originalPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if (playerentered == true)
+        {
+            transform.Translate(Vector3.down * fallDownSpeed * Time.deltaTime);
+            if (transform.position.y<whenToDestroy)
+            {
+                gameObject.GetComponent<MeshRenderer>().enabled = false;
+                StartCoroutine(reSpawn());
+            }
+        }
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="Player")
+        {
+            StartCoroutine(fallDown());
+        }
+    }
+
+    IEnumerator fallDown()
+    {
+        yield return new WaitForSeconds(timeBeforeDownFall);
+        playerentered = true;
+    }
+
+    IEnumerator reSpawn()
+    {
+        yield return new WaitForSeconds(2);
+        playerentered = false;
+        transform.position = originalPosition;
+        gameObject.GetComponent<MeshRenderer>().enabled = true;
+    }
+}
---
+++ Hotel_Platformer/Assets/Scripts/SelfRotateBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfRotateBehaviour : MonoBehaviour {
+    public bool rotateX;
+    public bool rotateY;
+    public bool rotateZ;
+    public float rotateSpeed;
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (rotateX==true)
+        {
+            transform.Rotate(Vector3.right*rotateSpeed*Time.deltaTime); //Rotate around x axis
+        }
+        else
+        {
+            if (rotateY==true)
+            {
+                transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime); //Rotate around y axis
+            }
+            else
+            {
+                if (rotateZ==true)
+                {
+                    transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime); //Rotate around z axis
+                }
+            }
+        }
+	}
+
+}
