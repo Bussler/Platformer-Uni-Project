d33059541e6af128c42b8af2eede0c05e4b48c7f
Luca <34301369+GilgusMaximus@users.noreply.github.com>
2018-01-09 22:28:48 +0100

    Portal Mechanic
    Added Brackeys Portalmechanism into the project. Atm there is the problem, that we do not have a 1st Person Camera, so the effect is lost. Will add it as soon as Possible

---
+++ Hotel_Platformer/Assets/Scripts/Portals/CameraPosition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosition : MonoBehaviour {
+
+    public Transform playerCamera;
+    public Transform portalPosition;
+    public Transform otherPortalPosition;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 PlayerOffsetFromPortal = playerCamera.position - otherPortalPosition.position;
+        transform.position = new Vector3(portalPosition.position.x + PlayerOffsetFromPortal.x, portalPosition.position.y, portalPosition.position.z + PlayerOffsetFromPortal.z);
+    }
+}
---
+++ Hotel_Platformer/Assets/Scripts/Portals/PortalTeleporterScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTeleporterScript : MonoBehaviour {
+
+    public Transform player;
+    public Transform receiver;
+    private bool playerIsOverlapping;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (playerIsOverlapping) {
+            Vector3 portalToPlayer = player.position - transform.position;
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+            if(dotProduct < 0f) {
+                float rotationDifference = -Quaternion.Angle(transform.rotation, receiver.rotation);
+                rotationDifference += 180;
+                player.Rotate(Vector3.up, rotationDifference);
+                Vector3 positionOffset = Quaternion.Euler(0f, rotationDifference,0f)* portalToPlayer;
+                player.position = receiver.position + positionOffset;
+                playerIsOverlapping = false;
+            }
+        }
+	}
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player") {
+            playerIsOverlapping = true;
+        }
+    }
+    private void OnTriggerExit(Collider other) {
+        if(other.tag == "Player") {
+            playerIsOverlapping = false;
+        }
+    }
+}
