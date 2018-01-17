56c423c8a6d71559f9ec367ca1be3109d533f06e
DavidDaffer <34895642+DavidDaffer@users.noreply.github.com>
2018-01-17 10:34:30 +0100

    Scripts fürs Level

--- Hotel_Platformer/Assets/Scenes/Building Level/Scripts/Spawn Building.cs
+++ Hotel_Platformer/Assets/Scenes/Building Level/Scripts/Spawn Building.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class SpawnBuilding : MonoBehaviour {
+    public GameObject toBeCloned;
+    public GameObject[] array;
+
 
 	// Use this for initialization
 	void Start () {
-		
+        array = new GameObject[100];
 	}
 	
 	// Update is called once per frame
