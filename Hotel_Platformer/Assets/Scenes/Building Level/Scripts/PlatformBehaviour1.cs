35f9db3f86b45f232e9d45195228d2f42c542c3d
DavidDaffer <34895642+DavidDaffer@users.noreply.github.com>
2018-01-20 22:58:17 +0100

    Mein LEvel angefangen

---
+++ Hotel_Platformer/Assets/Scenes/Building Level/Scripts/PlatformBehaviour1.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformBehaviour1 : MonoBehaviour {
+    private short dir;
+    private int counter;
+    private float speed;
+    private Vector3 pos;
+    private float smoothTime = 0.8f;
+    private Vector3 velocity= Vector3.up;
+    private Vector3 targetPosition;
+
+    void Start()
+    {
+        dir = 1;
+        counter = 0;
+         pos = transform.position;
+         targetPosition = new Vector3(pos.x, pos.y + 10f, pos.z);
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        if (transform.position.y >= targetPosition.y && velocity == Vector3.up)
+        {
+            velocity=Vector3.down;
+            targetPosition = new Vector3(targetPosition.x, targetPosition.y - 10f, targetPosition.z);
+        } else if(transform.position.y <= targetPosition.y && velocity == Vector3.down)
+        {
+            velocity = Vector3.up;
+            targetPosition = new Vector3(pos.x, pos.y - 10f, pos.z);
+        }
+        
+    }
+}
---
+++ Hotel_Platformer/Assets/Scenes/Building Level/Scripts/SpawnBuilding.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class SpawnBuilding : MonoBehaviour {
+    public Transform toBeCloned1;
+    public Transform toBeCloned2;
+    public GameObject[] array;
+
+
+	// Use this for initialization
+	void Start () {
+        array = new GameObject[100];
+        for(int i=0; i<100; i++)
+        {
+            float randomX = Random.Range(-100f, 100f);
+            float randomY = Random.Range(-100f, 0f);
+            float randomZ = Random.Range(-100f, 100f);
+            float randomObj = Random.Range(0f, 1f);
+
+            if (randomObj < .5f)
+            {
+                Instantiate(toBeCloned1, new Vector3(randomX, randomY, randomZ), new Quaternion(0f, 0f, 0f, 0f));
+            } else
+            {
+                Instantiate(toBeCloned2, new Vector3(randomX, randomY, randomZ), new Quaternion(0f, 0f, 0f, 0f));
+            }
+            
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
