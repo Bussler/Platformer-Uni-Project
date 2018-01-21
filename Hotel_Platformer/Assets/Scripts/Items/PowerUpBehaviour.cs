080a892e7b0350ebc873ca7ffdd987c3813b9df9
JoshuaWeggartner <34662787+JoshuaWeggartner@users.noreply.github.com>
2018-01-21 13:16:19 +0100

    power ups verbessert

--- Hotel_Platformer/Assets/Scripts/Items/PowerUpBehaviour.cs
+++ Hotel_Platformer/Assets/Scripts/Items/PowerUpBehaviour.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PowerUpBehaviour : MonoBehaviour {
 
@@ -10,9 +11,15 @@ public class PowerUpBehaviour : MonoBehaviour {
     public bool jumping;
 
     private PlayerMovmentTest playerScript;
-	
-	// Update is called once per frame
-	void Update () {
+
+    public GameObject text;
+    public GameObject t;
+    private void Start()
+    {
+         
+    }
+    // Update is called once per frame
+    void Update () {
 		
 	}
 
@@ -21,29 +28,47 @@ public class PowerUpBehaviour : MonoBehaviour {
        
         if (other.tag=="Player")//collision with the player
         {
+            
+            text.AddComponent<TextMesh>();
+            // text.transform.parent = GameObject.Find("LebenCanvas").transform;
+         t= Instantiate(text, this.transform.position,Quaternion.identity);
+            t.transform.position = this.transform.position+Vector3.up;
+            t.transform.LookAt(GameObject.FindObjectOfType<Camera>().transform);
+            t.transform.GetChild(0).GetComponent<TextMesh>().fontSize = 10;
+            t.transform.GetChild(0).GetComponent<TextMesh>().characterSize = 0.2f;
             Debug.Log("Power");
             playerScript =other.gameObject.GetComponent<PlayerMovmentTest>();
+            
 
             //enable abilities, this could also be used to disable abilities
             if (run==true)
             {
                 playerScript.hasAbilityRunning = true;
+                t.transform.GetChild(0).GetComponent<TextMesh>().text = "Running learned. Press 'C' to run";
             }
             if (doubleJump == true)
             {
                 playerScript.timesToJump++;
+                t.transform.GetChild(0).GetComponent<TextMesh>().text = "Doublejumping learned. Press 'Space' \nwhile in air to doublejump";
             }
             if (gliding==true)
             {
                 playerScript.hasAbilityGliding = true;
+                t.transform.GetChild(0).GetComponent<TextMesh>().text = "Giding learned. Press 'F' while in air to glide";
             }
             if (jumping==true)
             {
                 playerScript.hasAbilityJumping = true;
             }
-
-            Destroy(gameObject);
+            Invoke("Destroytext", 3);
+            this.GetComponent<MeshRenderer>().enabled = false;
+            this.GetComponent<SphereCollider>().enabled = false;
         }
     }
 
+    void Destroytext()
+    {
+        Destroy(t);
+        Destroy(gameObject);
+    }
 }
---
+++ Hotel_Platformer/Assets/Scripts/LoadNewLevel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNewLevel : MonoBehaviour
+{
+    public string LevelName;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "Player")//collision with the player
+        {
+            SceneManager.LoadScene("LevelName");
+
+        }
+    }
+}
