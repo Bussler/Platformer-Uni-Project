ebeb4a0656239d3c014da007f12c5e7fb2bc7009
Bussler <maarten.bussler@gmail.com>
2018-01-21 20:45:38 +0100

    fixed a bug in falling level

---
+++ Hotel_Platformer/Assets/MoveDown.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDown : MonoBehaviour {
+
+    public float speed;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.down * speed* Time.deltaTime);
+	}
+}
