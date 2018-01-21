9dc8ea7417a0b284d1e0e4533a64065c29ce18ec
DavidDaffer <34895642+DavidDaffer@users.noreply.github.com>
2018-01-21 01:31:58 +0100

    Level Scripts und Modelle

--- Hotel_Platformer/Assets/Scenes/Building Level/Scripts/PlatformBehaviour1.cs
+++ Hotel_Platformer/Assets/Scenes/Building Level/Scripts/PlatformBehaviour1.cs
@@ -3,36 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformBehaviour1 : MonoBehaviour {
-    private short dir;
-    private int counter;
-    private float speed;
-    private Vector3 pos;
-    private float smoothTime = 0.8f;
-    private Vector3 velocity= Vector3.up;
-    private Vector3 targetPosition;
+    public float threshold;
+    public float distance;
+    public float maxSpeed;
+    private float acceleration;
+    public float step;
+    private Vector3 startPos;
+    private Vector3 dir;
+    private Vector3 endPos;
 
     void Start()
     {
-        dir = 1;
-        counter = 0;
-         pos = transform.position;
-         targetPosition = new Vector3(pos.x, pos.y + 10f, pos.z);
+        startPos = transform.position;
+        dir = Vector3.up;
+        acceleration = 0F;
+        endPos = new Vector3(startPos.x, startPos.y + distance, startPos.z);
     }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
 
+    private void Update()
+    {
+        Debug.Log(acceleration);
+    }
 
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
-        if (transform.position.y >= targetPosition.y && velocity == Vector3.up)
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (acceleration < maxSpeed && transform.position.y<=endPos.y)
         {
-            velocity=Vector3.down;
-            targetPosition = new Vector3(targetPosition.x, targetPosition.y - 10f, targetPosition.z);
-        } else if(transform.position.y <= targetPosition.y && velocity == Vector3.down)
+            acceleration += step;
+        }
+        if (transform.position.y >= endPos.y - threshold  )
         {
-            velocity = Vector3.up;
-            targetPosition = new Vector3(pos.x, pos.y - 10f, pos.z);
+            acceleration -= step;
         }
         
+
+        transform.Translate(0, dir.y * acceleration, 0);
+
+        if (transform.position.y >= endPos.y)
+        {
+            acceleration = acceleration -step;
+        }
+
     }
 }
