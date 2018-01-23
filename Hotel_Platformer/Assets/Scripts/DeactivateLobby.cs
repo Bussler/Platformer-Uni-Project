74e729cd0e093a1e02111fc8d92c91b59c53dc78
Luca <34301369+GilgusMaximus@users.noreply.github.com>
2018-01-23 20:08:36 +0100

    Made a lot of changes BoyZZZZ

---
+++ Hotel_Platformer/Assets/Scripts/DeactivateLobby.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateLobby : MonoBehaviour {
+
+    public GameObject Lobby;
+    public GameObject wand;
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player") {
+            Lobby.SetActive(false);
+            wand.SetActive(true);
+        }
+    }
+}
---
+++ Hotel_Platformer/Assets/Scripts/LobbyTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyTrigger : MonoBehaviour {
+
+    public GameObject wand;
+    public GameObject Level;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            wand.GetComponent<MeshRenderer>().enabled = false;
+            wand.GetComponent<Collider>().enabled = false;
+            Level.SetActive(true);
+        }    
+    }
+
+
+
+}
