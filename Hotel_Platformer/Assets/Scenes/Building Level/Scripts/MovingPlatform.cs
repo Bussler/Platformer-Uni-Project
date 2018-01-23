1a5555687dda6b3818bdbb7abe71c19f5a6c0a85
DavidDaffer <34895642+DavidDaffer@users.noreply.github.com>
2018-01-23 02:00:25 +0100

    Level almosz done

---
+++ Hotel_Platformer/Assets/Scenes/Building Level/Scripts/MovingPlatform.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+    public GameObject clone;
+    public Transform[] points;
+    public float Speed;
+    public int index=0;
+    private int platFormState;
+    private int count;
+
+	// Use this for initialization
+	void Start () {
+        count = 1;
+        this.transform.position = points[index].transform.position;
+        index++;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(this.transform.position != points[index].transform.position)
+        {
+
+            this.transform.Translate(new Vector3(points[index].position.x - this.transform.position.x, points[index].position.y - this.transform.position.y, points[index].position.z - this.transform.position.z).normalized*Speed*Time.deltaTime);
+
+
+        } 
+        if((this.transform.position.x >= points[index].transform.position.x-0.5 && this.transform.position.x <= points[index].transform.position.x + 0.5)&&
+           ( this.transform.position.y >= points[index].transform.position.y - 0.5 && this.transform.position.y <= points[index].transform.position.y + 0.5)&&
+           ( this.transform.position.z >= points[index].transform.position.z - 0.5 && this.transform.position.z <= points[index].transform.position.z + 0.5))
+        {
+            index++;
+            if(index> points.Length-1)
+            {
+                index = 0;
+            }
+
+        }
+
+
+
+	}
+    public void OnDrawGizmos()  //Drawing the Raycasts for walljump in the editor
+    { for (int i = 0; i < points.Length;i++) {
+            Vector3 r= new Vector3(0,0,0);
+            if (i + 1 < points.Length)
+            {
+                 r = points[i+1].position - points[i ].position;
+            }
+            else
+            {
+                r = points[0].position - points[i].position;
+            }
+
+            
+            Gizmos.DrawRay(points[i].position, r);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if(other.tag=="Player" && count>1 && count < 5)
+        {
+            Debug.Log("Hit");
+            GameObject obj = Instantiate(clone, new Vector3(transform.position.x, transform.position.y - 20, transform.position.z + 15), Quaternion.identity);
+            obj.GetComponent<MovingPlatform>().Speed = 10;
+            count++;
+        } else if (other.tag == "Player" && count == 1)
+        {
+            Debug.Log("Hit");
+            GameObject obj = Instantiate(clone, new Vector3(transform.position.x, transform.position.y - 20, transform.position.z + 15), Quaternion.identity);
+            obj.GetComponent<MovingPlatform>().Speed = 0;
+            count++;
+        }
+        Debug.Log("Hit");
+    }
+
+
+}
